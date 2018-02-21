b9bfb60e55bccf9cbf1cc70188da8834df1f3390
Christian Fecteau <christian.fecteau@gmail.com>
2018-02-21 18:50:28 -0500

    docker

--- create-machine.sh
+++ create-machine.sh
@@ -5,6 +5,7 @@ set -eu
 [ -f ".travis.yml" ] || { echo >&2 "Please cd into the bundle before running this script."; exit 1; }
 
 docker --version;
+docker-compose --version;
 docker-machine --version;
 
 #docker-machine create \
