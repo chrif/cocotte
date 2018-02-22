3f18a42cf31adcf480bd0a12d7f9a3ae99fa9190
Christian Fecteau <christian.fecteau@gmail.com>
2018-02-21 21:00:16 -0500

    docker

--- create-machine.sh
+++ create-machine.sh
@@ -4,14 +4,16 @@ set -eu
 
 [ -f ".travis.yml" ] || { echo >&2 "Please cd into the bundle before running this script."; exit 1; }
 
-docker --version;
-docker-compose --version;
-docker-machine --version;
+docker-machine rm --force ${MACHINE_NAME};
 
-#docker-machine create \
-#	--driver digitalocean \
-#	--digitalocean-access-token ${DIGITAL_OCEAN_API_TOKEN} \
-#	--engine-opt log-driver="json-file" \
-#	--engine-opt log-opt="max-size=1m" \
-#	--engine-opt log-opt="max-file=10" \
-#	${MACHINE_NAME}
+docker-machine create \
+	--driver digitalocean \
+	--digitalocean-access-token ${DIGITAL_OCEAN_API_TOKEN} \
+	--engine-opt log-driver="json-file" \
+	--engine-opt log-opt="max-size=1m" \
+	--engine-opt log-opt="max-file=10" \
+	${MACHINE_NAME};
+
+docker-machine ls | grep "${MACHINE_NAME}     -        digitalocean   Running" >/dev/null 2>&1 || exit 1;
+
+docker-machine rm --force ${MACHINE_NAME};
