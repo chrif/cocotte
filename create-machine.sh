19dd45ecf0bb787c849fa4f439ff3a6c80e870e2
Christian Fecteau <christian.fecteau@gmail.com>
2018-02-21 21:13:06 -0500

    docker

--- create-machine.sh
+++ create-machine.sh
@@ -4,8 +4,6 @@ set -eu
 
 [ -f ".travis.yml" ] || { echo >&2 "Please cd into the bundle before running this script."; exit 1; }
 
-docker-machine rm -y ${MACHINE_NAME};
-
 docker-machine create \
 	--driver digitalocean \
 	--digitalocean-access-token ${DIGITAL_OCEAN_API_TOKEN} \
