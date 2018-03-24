d4007b48bd11a3a60c2f135dba4c2fd54aeb3ba2
Christian Fecteau <christian.fecteau@gmail.com>
2018-03-24 12:17:50 -0400

    test travis

--- installer/create-machine.sh
+++ installer/create-machine.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-[ -f ".travis.yml" ] || { echo >&2 "Please cd into the bundle before running this script."; exit 1; }
-
 docker-machine create \
 	--driver digitalocean \
 	--digitalocean-access-token ${DIGITAL_OCEAN_API_TOKEN} \
---
+++ installer/remove-machine.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -eu
+
+docker-machine rm -y ${MACHINE_NAME};
