b188e4aa15e8f32927c576ed75281c615e55da59
Christian Fecteau <christian.fecteau@gmail.com>
2018-03-25 16:31:20 -0400

    fix test

--- installer/cocotte-entrypoint.sh
+++ installer/cocotte-entrypoint.sh
@@ -4,17 +4,9 @@ set -e
 
 export MACHINE_IP=$(docker-machine -s ${MACHINE_STORAGE_PATH} inspect --format='{{.Driver.IPAddress}}' ${MACHINE_NAME})
 
-if [ "$1" = 'create' ]; then
-    exec sh create;
-elif [ "$1" = 'remove' ]; then
-    exec sh remove;
-elif [ "$1" = 'console' ]; then
-    shift;
-    exec php/bin/console "$@";
-elif [ "$1" = 'phpunit' ]; then
-    shift;
-    cd php;
-    exec php vendor/bin/phpunit "$@";
-else
-    exec "$@";
+if [ "$1" = 'create' ]; then exec sh create
+elif [ "$1" = 'remove' ]; then exec sh remove
+elif [ "$1" = 'console' ]; then shift; exec php/bin/console "$@";
+elif [ "$1" = 'phpunit' ]; then shift; cd php; exec php vendor/bin/phpunit "$@";
+else exec "$@";
 fi
