ae314b2c33fb2e6acefae9fe1acf5d752f444add
Christian Fecteau <christian.fecteau@gmail.com>
2018-03-27 19:02:58 -0400

    traefik works in dev

--- installer/traefik/docker/bin/dev.sh
+++ installer/traefik/docker/bin/dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sh docker/bin/rm.sh
 sh docker/bin/init.sh
-eval $(cat .env .env-override) docker-compose up -d --build traefik
+eval $(cat .env .env-override) docker-compose up --build traefik
