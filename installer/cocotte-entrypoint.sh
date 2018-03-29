1c8775e68a954a64ba0511e3904a5219f5bc5e2f
Christian Fecteau <christian.fecteau@gmail.com>
2018-03-29 15:57:55 -0400

    - made machine storage path saved in config the absolute path on host, not on installer

--- installer/cocotte-entrypoint.sh
+++ installer/cocotte-entrypoint.sh
@@ -2,6 +2,19 @@
 
 set -eu
 
+if [ "$MACHINE_STORAGE_PATH" != "/host/machine" ]; then
+
+	if [ -e $MACHINE_STORAGE_PATH ] && ! [ -L $MACHINE_STORAGE_PATH ]; then
+		echo  "Error: cannot symlink MACHINE_STORAGE_PATH to '$MACHINE_STORAGE_PATH' because it is a real path on installer. Start installer from a different directory on your computer."
+		exit 1
+	fi
+
+	mkdir -pv $(dirname $MACHINE_STORAGE_PATH)
+	mkdir -pv /host/machine
+	ln -sfnv /host/machine ${MACHINE_STORAGE_PATH}
+
+fi
+
 if [ "$1" = 'create' ]; then exec sh create
 elif [ "$1" = 'remove' ]; then exec sh remove
 elif [ "$1" = 'console' ]; then shift; cd php; exec bin/console "$@";
