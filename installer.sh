df7d9a5dc135503ae2dce73bd45c8bde797c35a6
Christian Fecteau <christian.fecteau@gmail.com>
2018-03-28 20:37:25 -0400

    fix install traefik on host

--- installer.sh
+++ installer.sh
@@ -2,8 +2,18 @@
 
 set -eu
 
+if [ "$1" = 'test' ]; then
+    set -a
+    . ../.env
+    set +a
+    shift
+fi
+
 docker run --rm \
     -e DIGITAL_OCEAN_API_TOKEN=${DIGITAL_OCEAN_API_TOKEN} \
     -e MACHINE_NAME=${MACHINE_NAME} \
+    -e TRAEFIK_ACME_EMAIL=${TRAEFIK_ACME_EMAIL} \
+    -e TRAEFIK_AUTH_BASIC=${TRAEFIK_AUTH_BASIC} \
+    -e TRAEFIK_UI_HOST=${TRAEFIK_UI_HOST} \
     -v "$(pwd)":/host \
     chrif/cocotte $@
