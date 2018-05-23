2f33327c2d391d803ddad74154f1b72fa86ed826
Christian Fecteau <christian.fecteau@gmail.com>
2018-05-22 21:16:34 -0400

    Scrutinizer fixes

--- installer/src/DependencyInjection/LazyEnvironmentPass.php
+++ installer/src/DependencyInjection/LazyEnvironmentPass.php
@@ -8,6 +8,7 @@ use Cocotte\Environment\LazyEnvironmentValue;
 use Cocotte\Shell\Env;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
 final class LazyEnvironmentPass implements CompilerPassInterface
@@ -19,28 +20,43 @@ final class LazyEnvironmentPass implements CompilerPassInterface
         $lazyLoaderDefinition = $container->getDefinition(LazyEnvironmentLoader::class);
 
         foreach ($configs as $id => $attributes) {
-            $valueDefinition = $container->getDefinition($id);
-            $valueClass = $container->getParameterBag()->resolveValue($valueDefinition->getClass());
-            $reflectionValueClass = $container->getReflectionClass($valueClass);
+            $this->processTag($container, $id, $lazyLoaderDefinition);
+        }
+    }
 
-            if (!$reflectionValueClass->implementsInterface(LazyEnvironmentValue::class)) {
-                throw new \Exception(
-                    "$valueClass does not implement ".LazyEnvironmentValue::class
-                );
-            }
+    private function processTag(ContainerBuilder $container, string $id, Definition $lazyLoaderDefinition): void
+    {
+        $valueDefinition = $container->getDefinition($id);
+        $valueClass = $container->getParameterBag()->resolveValue($valueDefinition->getClass());
+        $reflectionValueClass = $container->getReflectionClass($valueClass);
 
-            if (!$valueDefinition->getFactory()) {
-                if (!$reflectionValueClass->implementsInterface(FromEnvLazyFactory::class)) {
-                    throw new \Exception(
-                        "There is not custom factory and $valueClass does not implement ".FromEnvLazyFactory::class
-                    );
-                }
-                $valueDefinition->setFactory([$valueClass, "fromEnv"]);
-                $valueDefinition->setArguments([new Reference(Env::class)]);
-            }
+        $this->assertLazyEnvironmentValue($reflectionValueClass, $valueClass);
 
-            $valueDefinition->setLazy(true);
-            $lazyLoaderDefinition->addMethodCall('registerValue', [new Reference($id)]);
+        if (!$valueDefinition->getFactory()) {
+            $this->assertCustomFactory($reflectionValueClass, $valueClass);
+            $valueDefinition->setFactory([$valueClass, "fromEnv"]);
+            $valueDefinition->setArguments([new Reference(Env::class)]);
+        }
+
+        $valueDefinition->setLazy(true);
+        $lazyLoaderDefinition->addMethodCall('registerValue', [new Reference($id)]);
+    }
+
+    private function assertLazyEnvironmentValue(\ReflectionClass $reflectionValueClass, string $valueClass): void
+    {
+        if (!$reflectionValueClass->implementsInterface(LazyEnvironmentValue::class)) {
+            throw new \Exception(
+                "$valueClass does not implement ".LazyEnvironmentValue::class
+            );
+        }
+    }
+
+    private function assertCustomFactory(\ReflectionClass $reflectionValueClass, string $valueClass): void
+    {
+        if (!$reflectionValueClass->implementsInterface(FromEnvLazyFactory::class)) {
+            throw new \Exception(
+                "There is not custom factory and $valueClass does not implement ".FromEnvLazyFactory::class
+            );
         }
     }
 }
