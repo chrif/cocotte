32d41ecece0a63c6a2bd6994c121a25a02b7000b
Christian Fecteau <christian.fecteau@gmail.com>
2018-05-22 21:45:18 -0400

    Scrutinizer fixes

--- installer/src/Console/MarkdownDescriptor.php
+++ installer/src/Console/MarkdownDescriptor.php
@@ -48,12 +48,10 @@ class MarkdownDescriptor
     {
         $description = $this->removeDecoration($argument->getDescription());
         $this->write(
-            '#### `'.($argument->getName() ?: '<none>')."`\n\n"
-            .($description ? preg_replace('/\s*[\r\n]\s*/',
-                    "\n",
-                    $description)."\n\n" : '')
-            .'* Is required: '.($argument->isRequired() ? 'yes' : 'no')."\n"
-            .'* Is array: '.($argument->isArray() ? 'yes' : 'no')."\n"
+            '#### `'.$this->argumentName($argument)."`\n\n"
+            .$this->formatArgumentDescription($description)
+            .'* Is required: '.$this->argumentIsRequired($argument)."\n"
+            .'* Is array: '.$this->argumentIsArray($argument)."\n"
             .'* Default: `'.str_replace("\n", '', var_export($argument->getDefault(), true)).'`'
         );
     }
@@ -65,41 +63,24 @@ class MarkdownDescriptor
             $name .= '|-'.implode('|-', explode('|', $option->getShortcut())).'';
         }
 
-        $emphasis = $option instanceof StyledInputOption ? '##### ' : '';
+        $emphasis = $this->optionEmphasis($option);
 
         $description = $this->removeDecoration($option->getDescription());
         $this->write(
             '#### `'.$name.'`'."\n\n"
-            .($description ? $emphasis.preg_replace('/\s*[\r\n]\s*/', "\n", $description)."\n\n" : '')
-            .'* Accept value: '.($option->acceptValue() ? 'yes' : 'no')."\n"
-            .'* Is value required: '.($option->isValueRequired() ? 'yes' : 'no')."\n"
-            .'* Is multiple: '.($option->isArray() ? 'yes' : 'no')."\n"
+            .$this->formatOptionDescription($description, $emphasis)
+            .'* Accept value: '.$this->optionValue($option)."\n"
+            .'* Is value required: '.$this->optionValueRequired($option)."\n"
+            .'* Is multiple: '.$this->optionIsMultiple($option)."\n"
             .'* Default: `'.str_replace("\n", '', var_export($option->getDefault(), true)).'`'
         );
     }
 
     private function describeInputDefinition(InputDefinition $definition)
     {
-        if ($showArguments = count($definition->getArguments()) > 0) {
-            $this->write('### Arguments');
-            foreach ($definition->getArguments() as $argument) {
-                $this->write("\n\n");
-                $this->describeInputArgument($argument);
-            }
-        }
+        $showArguments = $this->describeInputDefinitionArguments($definition);
 
-        $inputOptions = $this->getInputOptions($definition);
-        if (count($inputOptions) > 0) {
-            if ($showArguments) {
-                $this->write("\n\n");
-            }
-
-            $this->write('### Options');
-            foreach ($inputOptions as $option) {
-                $this->write("\n\n");
-                $this->describeInputOption($option);
-            }
-        }
+        $this->describeInputDefinitionOptions($definition, $showArguments);
     }
 
     private function describeCommand(Command $command)
@@ -136,28 +117,9 @@ class MarkdownDescriptor
 
         $this->write($title."\n".str_repeat('=', Helper::strlen($title)));
 
-        $commands = array_filter($description->getCommands(),
-            function (Command $command) {
-                return $command instanceof DocumentedCommand;
-            });
+        $this->tableOfContents($description);
 
-        $this->write("\n\n");
-        $this->write(implode("\n",
-                array_map(
-                    function (Command $command) {
-                        return sprintf(
-                            "* [`%s`](#%s)\n  > %s",
-                            $command->getName(),
-                            str_replace(':', '', $command->getName()),
-                            $this->removeDecoration($command->getDescription())
-                        );
-                    },
-                    $commands
-                )
-            )
-        );
-
-        foreach ($commands as $command) {
+        foreach ($this->filterCommands($description) as $command) {
             $this->write("\n\n---\n\n");
             $this->describeCommand($command);
         }
@@ -177,13 +139,7 @@ class MarkdownDescriptor
     private function getInputOptions(InputDefinition $definition)
     {
         $options = $definition->getOptions();
-        $options = array_filter($options,
-            function (InputOption $option) {
-                return !in_array(
-                    $option->getName(),
-                    ['help', 'quiet', 'verbose', 'version', 'ansi', 'no-ansi', 'no-interaction']
-                );
-            });
+        $options = $this->filterOptions($options);
         usort($options,
             function (InputOption $a, InputOption $b) {
                 $aRequired = $a->isValueRequired() ? 1 : 0;
@@ -196,5 +152,175 @@ class MarkdownDescriptor
             });
 
         return $options;
+    }
+
+    /**
+     * @param $description
+     * @param $emphasis
+     * @return string
+     */
+    private function formatOptionDescription($description, $emphasis): string
+    {
+        return ($description ? $emphasis.preg_replace('/\s*[\r\n]\s*/', "\n", $description)."\n\n" : '');
+    }
+
+    /**
+     * @param InputOption $option
+     * @return string
+     */
+    private function optionValue(InputOption $option): string
+    {
+        return ($option->acceptValue() ? 'yes' : 'no');
+    }
+
+    /**
+     * @param InputOption $option
+     * @return string
+     */
+    private function optionValueRequired(InputOption $option): string
+    {
+        return ($option->isValueRequired() ? 'yes' : 'no');
+    }
+
+    /**
+     * @param InputOption $option
+     * @return string
+     */
+    private function optionIsMultiple(InputOption $option): string
+    {
+        return ($option->isArray() ? 'yes' : 'no');
+    }
+
+    /**
+     * @param InputOption $option
+     * @return string
+     */
+    private function optionEmphasis(InputOption $option): string
+    {
+        return $option instanceof StyledInputOption ? '##### ' : '';
+    }
+
+    /**
+     * @param InputArgument $argument
+     * @return string
+     */
+    private function argumentIsRequired(InputArgument $argument): string
+    {
+        return ($argument->isRequired() ? 'yes' : 'no');
+    }
+
+    /**
+     * @param InputArgument $argument
+     * @return string
+     */
+    private function argumentIsArray(InputArgument $argument): string
+    {
+        return ($argument->isArray() ? 'yes' : 'no');
+    }
+
+    /**
+     * @param InputArgument $argument
+     * @return string
+     */
+    private function argumentName(InputArgument $argument): string
+    {
+        return ($argument->getName() ?: '<none>');
+    }
+
+    /**
+     * @param $description
+     * @return string
+     */
+    private function formatArgumentDescription($description): string
+    {
+        return ($description ? preg_replace('/\s*[\r\n]\s*/',
+                "\n",
+                $description)."\n\n" : '');
+    }
+
+    /**
+     * @param InputDefinition $definition
+     * @return bool
+     */
+    private function describeInputDefinitionArguments(InputDefinition $definition): bool
+    {
+        if ($showArguments = count($definition->getArguments()) > 0) {
+            $this->write('### Arguments');
+            foreach ($definition->getArguments() as $argument) {
+                $this->write("\n\n");
+                $this->describeInputArgument($argument);
+            }
+        }
+
+        return $showArguments;
+    }
+
+    /**
+     * @param InputDefinition $definition
+     * @param $showArguments
+     */
+    private function describeInputDefinitionOptions(InputDefinition $definition, $showArguments): void
+    {
+        $inputOptions = $this->getInputOptions($definition);
+        if (count($inputOptions) > 0) {
+            if ($showArguments) {
+                $this->write("\n\n");
+            }
+
+            $this->write('### Options');
+            foreach ($inputOptions as $option) {
+                $this->write("\n\n");
+                $this->describeInputOption($option);
+            }
+        }
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Descriptor\ApplicationDescription $description
+     * @return array
+     */
+    private function filterCommands($description): array
+    {
+        return array_filter($description->getCommands(),
+            function (Command $command) {
+                return $command instanceof DocumentedCommand;
+            });
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Descriptor\ApplicationDescription $description
+     */
+    private function tableOfContents($description): void
+    {
+        $this->write("\n\n");
+        $this->write(implode("\n",
+                array_map(
+                    function (Command $command) {
+                        return sprintf(
+                            "* [`%s`](#%s)\n  > %s",
+                            $command->getName(),
+                            str_replace(':', '', $command->getName()),
+                            $this->removeDecoration($command->getDescription())
+                        );
+                    },
+                    $this->filterCommands($description)
+                )
+            )
+        );
+    }
+
+    /**
+     * @param $options
+     * @return array
+     */
+    private function filterOptions($options): array
+    {
+        return array_filter($options,
+            function (InputOption $option) {
+                return !in_array(
+                    $option->getName(),
+                    ['help', 'quiet', 'verbose', 'version', 'ansi', 'no-ansi', 'no-interaction']
+                );
+            });
     }
 }
