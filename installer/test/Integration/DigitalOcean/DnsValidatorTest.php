cadb551f0021bd46705e424e586ff1e309a0c1da
Christian Fecteau <christian.fecteau@gmail.com>
2021-06-13 14:20:19 -0400

    Remove project domain

--- installer/test/Integration/DigitalOcean/DnsValidatorTest.php
+++ installer/test/Integration/DigitalOcean/DnsValidatorTest.php
@@ -8,16 +8,6 @@ use Cocotte\Test\Collaborator\DigitalOcean\DnsValidatorActual;
 
 final class DnsValidatorTest extends ApplicationTestCase
 {
-    public function test_it_validates_my_rocks_domain()
-    {
-        $validator = DnsValidatorActual::get($this->container())->service();
-        $this->expectExceptionMessageRegExp(
-            "#Failed to validate name servers for 'cocotte\.rocks':\n".
-            "'ns-cloud-a[1-4]\.googledomains\.com' is not a Digital Ocean's name server in the form of ".
-            "ns\[0-9\]\.digitalocean\.com#"
-        );
-        $validator->validateHost(Hostname::parse('cocotte.rocks'));
-    }
 
     public function test_it_does_not_fail_with_fsockopen_unable_to_connect_for_rocks_domains()
     {
